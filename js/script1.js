2692daa20ba21024cf875d76f7f0062216c2fca5
SolPomares <168312529+SolPomares@users.noreply.github.com>
2024-11-23 15:57:58 -0300

    Update script1.js

--- js/script1.js
+++ js/script1.js
@@ -1,18 +1,24 @@
 var botonPlay = document.getElementById("botonPlay");
 
-botonPlay.addEventListener("click", function() {
-  // Agregar la clase para la animación de "encoger"
-  botonPlay.classList.add("shrink"); 
-  // Obtener todos los elementos de video dentro del canvas de p5.js
-  videos = document.querySelectorAll('canvas video'); 
 
-  videos.forEach(function(video) {
-    vid.muted = true; // Silenciar los videos inicialmente
-    vid1.muted = true;
-    vid.play(); 
-    vid1.play();
-    vid.loop();
-    vid1.loop();
+document.addEventListener('DOMContentLoaded', function() {
+  var botonPlay = document.getElementById("botonPlay");
+
+  botonPlay.addEventListener("click", function() {
+      // Agregar la clase para la animación de "encoger"
+    botonPlay.classList.add("shrink"); 
+    // Obtener todos los elementos de video dentro del canvas de p5.js
+    videos = document.querySelectorAll('canvas video'); 
   
-  }); 
+    videos.forEach(function(video) {
+      vid.muted = true; // Silenciar los videos inicialmente
+      vid1.muted = true;
+      vid.play(); 
+      vid1.play();
+      vid.loop();
+      vid1.loop();
+    
+    }); 
+
+  });
 });
